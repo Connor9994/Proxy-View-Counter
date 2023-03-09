a9e5c0239fdec689d89f172c4598d31e2b713376
Connor <39637206+Connor9994@users.noreply.github.com>
2023-03-08 22:31:46 -0800

    Add files via upload

--- RestartChrome.py
+++ RestartChrome.py
@@ -2,6 +2,9 @@ import subprocess
 import time
 import asyncio
 import sched
+import os
+import subprocess
+
 print("Starting RestartChrome.py")
 
 async def CrashChrome():
@@ -10,4 +13,11 @@ async def CrashChrome():
 
 while True:
     asyncio.run(CrashChrome())
+    #check if we have lost connection to the workgroup
+    connection = os.popen("ping -n 1 Laptop").read()
+    print(connection)
+    if "Ping request could not find host" in connection: 
+        os.system("shutdown /r /t 0")
+    else:
+        print("still connected to the wifi") 
     time.sleep(900)
